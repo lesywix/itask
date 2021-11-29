eafabd543c5ff8dc65ecc0a6d94c7000ffd5983c
lesywix <lin.wx@foxmail.com>
2021-11-29 23:43:20 +0800

    Add

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
